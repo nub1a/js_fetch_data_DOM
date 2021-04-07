6cbaa3d3ad2709a79c82dacc9350d8557fdfa53c
Владислав Сандык <nubia@MacBook-Pro-Vladislav.local>
2021-04-07 19:19:51 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const url = 'https://mate-academy.github.io/phone-catalogue-static/api';
+
+const listOfPhones = document.createElement('ul');
+
+document.body.append(listOfPhones);
+
+function getPhonesFromApi() {
+  return fetch(`${url}/phones.json`);
+}
+
+function getPhones() {
+  return new Promise((resolve, reject) => {
+    getPhonesFromApi()
+      .then(response => response.json())
+      .then(data => resolve(data));
+
+    setTimeout(() => reject(
+      new Error('error: No answer from server.')
+    ), 5000);
+  });
+}
+
+getPhones()
+  .then(phones => {
+    phones.forEach(phone => {
+      const phoneListItem = document.createElement('li');
+
+      phoneListItem.textContent = phone.name;
+      listOfPhones.append(phoneListItem);
+    });
+  });
